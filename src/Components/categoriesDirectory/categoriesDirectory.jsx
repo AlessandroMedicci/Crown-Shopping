ee341c7a69629e6155e4fc410c148ddcccb56e83
AlessandroMedicci <alessandro.medicci@gmail.com>
2022-10-01 21:15:36 +0400

    First Commit

---
+++ src/Components/categoriesDirectory/categoriesDirectory.jsx
@@ -0,0 +1,14 @@
+import "./categoriesDirectory.scss";
+import CategoryItem from "../categoryItem/categoryItem";
+
+const CategoriesDirectory = ({ categories }) => {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default CategoriesDirectory;
---
+++ src/Routes/navigation/navigation.jsx
@@ -0,0 +1,24 @@
+import { Fragment } from "react";
+import "./navigation.scss";
+import { ReactComponent as CrownLogo } from "../../Assets/crown.svg";
+import { Outlet, Link } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrownLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
