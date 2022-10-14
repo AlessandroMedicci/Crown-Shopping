e1b60bf34c53386728bfd64a3b6c17ce7c233920
AlessandroMedicci <alessandro.medicci@gmail.com>
2022-10-14 14:47:00 +0400

    Redux-Persist added

--- src/index.js
+++ src/index.js
@@ -4,23 +4,22 @@ import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-//import { CategoriesContextProvider } from "./Context/categoriesContext";
-//import { CartDropdownContextProvider } from "./Context/cartItemContext";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import { store } from "./Store/store";
+import { persistor } from "./Store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
