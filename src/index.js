d329bc83a39c784b37e9d5d6d611ea55b2971981
AlessandroMedicci <alessandro.medicci@gmail.com>
2022-10-23 19:40:28 +0400

    Debugging

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
@@ -25,5 +24,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
-reportWebVitals();
